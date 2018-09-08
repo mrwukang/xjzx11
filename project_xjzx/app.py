ba65921900039755a970d10661586173db641308
wukang <wukangmr@163.com>
2018-09-08 19:59:00 +0800

    shenm

--- project_xjzx/app.py
+++ project_xjzx/app.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from flask import render_template
-import redis
-from views_admin import admin_blueprint
-from views_user import user_blueprint
-from views_news import news_blueprint
+
 
 def create_app(Config):
     app = Flask(__name__)
