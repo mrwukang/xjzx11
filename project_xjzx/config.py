d074f2103674c94e106321e0a80e3e69820ef247
wukang <wukangmr@163.com>
2018-09-08 21:51:22 +0800

    项目第一天
    修改配置文件

--- project_xjzx/config.py
+++ project_xjzx/config.py
@@ -3,21 +3,10 @@ import os
 
 class Config(object):
     DEBUGE = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://wukang:123587415@192.168.15.131:3306/database"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    REDIS_HOST = '192.168.15.131'
-    REDIS_PORT = 6379
-    SECRET_KEY = "itheima"
-    SESSION_TYPE = "redis"
-    SESSION_USE_SIGNER = True
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    PERMANENT_SESSION_LIFETIME = 60*60*24*14
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    QINIU_AK = 'H999S3riCJGPiJOity1GsyWufw3IyoMB6goojo5e'
-    QINIU_SK = 'uOZfRdFtljIw7b8jr6iTG-cC6wY_-N19466PXUAb'
-    QINIU_BUCKET = 'itcast20171104'
-    QINIU_URL = 'http://oyvzbpqij.bkt.clouddn.com/'
+
 
 class DevelopConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://wukang:123587415@192.168.15.131:3306/xjzx"
+
+
--- project_xjzx/manage.py
+++ project_xjzx/manage.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from redis import StrictRedis
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
+
+
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://wukang:123587415@192.168.15.131:3306/database"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "jpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq"
+    REDIS_HOST = "192.168.15.131"
+    REDIS_PORT = 6379
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+CSRFProtect(app)
+redis_store = StrictRedis(host=Config.REDIS_PORT, port=Config.REDIS_PORT)
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command("db", MigrateCommand)
 
 
 @app.route("/")
@@ -11,4 +34,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
