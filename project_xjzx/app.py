a49f14ad7fbdf8e30212d27aba8625f739c23263
wukang <wukangmr@163.com>
2018-09-09 23:13:17 +0800

    ddd

--- project_xjzx/app.py
+++ project_xjzx/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("news/index.html")
+    return render_template("admin/index.html")
 
 
 if __name__ == '__main__':
