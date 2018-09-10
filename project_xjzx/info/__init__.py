fb7efce2d680dd39916e1d71bf6ad09af501b92d
wukang <wukangmr@163.com>
2018-09-10 15:31:27 +0800

    配置models

--- project_xjzx/info/__init__.py
+++ project_xjzx/info/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -16,17 +17,27 @@ redis_store = None
 
 def setup_log(config_name):
     """配置日志"""
-    logging.basicConfig(level=config[config_name].LOG_LEVEL)
-    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
-    formater = logging.Formatter("%(levelname)s %(filename)s:%(lineno)d %(message)s")
-    file_log_handler.setFormatter(formater)
+    dir_file = os.path.abspath(__file__)
+    dir_info = os.path.dirname(dir_file)
+    dir_base = os.path.dirname(dir_info)
+    dir_log = os.path.join(dir_base, 'logs/log')
+
+    # 设置日志的记录等级
+    logging.basicConfig(level=config[config_name].LOG_LEVEL)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler(dir_log, maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
     logging.getLogger().addHandler(file_log_handler)
 
 
 def create_app(config_name):
     """通过传入不同的配置名称，初始化对应配置的应用实例"""
 
-    # setup_log(config_name)
+    setup_log(config_name)
     app = Flask(__name__)
     from info.modules.news import news_blue
     app.register_blueprint(news_blue)
--- project_xjzx/manage.py
+++ project_xjzx/manage.py
@@ -3,14 +3,15 @@
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import create_app, db
+from info import create_app
+from info.models import db
 
 
 app = create_app("development")
 manager = Manager(app)
 # 数据库迁移
-# Migrate(app, db)
-# manager.add_command("db", MigrateCommand)
+Migrate(app, db)
+manager.add_command("db", MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
